dde5073fba26eaa9f361b84680e0bb6d8e9e990d
Victor Adeyanju <adeyanju.bincom@gmail.com>
2019-07-23 18:20:46 +0100

    updated requirement.txt

--- Django/theHatch2019/settings.py
+++ Django/theHatch2019/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'InsureMe.apps.InsuremeConfig',
+    'rest_frame',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Lagos'
 
 USE_I18N = True
 
