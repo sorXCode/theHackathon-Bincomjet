25268b60cbc8198a03e32520ae71fea090c47eb3
benneee <benedictiknkeonye@gmail.com>
2019-07-25 01:39:39 +0100

    Completed markup and preliminary logic for auth

--- InsureMe/src/app/@theme/components/register/register.component.ts
+++ InsureMe/src/app/@theme/components/register/register.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
@@ -7,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  reg_form: FormGroup;
   constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.reg_form = new FormGroup({
+      email: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+      r_password: new FormControl('', Validators.required)
+    });
+  }
+
+  register() {
+    const password = this.reg_form.value.password;
+    const repeated = this.reg_form.value.r_password;
+    if (password !== repeated) {
+      console.error('Passwords do not match!');
+    }
+    console.log('[form values]:', this.reg_form.value);
+  }
   gotoLogin() {
     this.router.navigate(['/login'], { relativeTo: this.activatedRoute });
+  }
+  goBack() {
+    this.router.navigate(['/'], { relativeTo: this.activatedRoute });
   }
 }
