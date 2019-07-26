02f4a8ea92ab2103864c54c7234d0da967e26438
Christopher Akaninyene Okoro <uraniumkid30@gmail.com>
2019-07-26 17:37:53 +0100

    Update serializers.py

--- insuretech_apis/serializers.py
+++ insuretech_apis/serializers.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User, Group
-from rest_framework import serializers, viewsets
+from rest_framework import serializers
 from .models import *
-
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -14,43 +13,57 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
         model = Group
         fields = ['url', 'name']
 
-
-class ProfileLoginSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AInsuranceUserProfile
-        fields = '__all__'  # ['username','password']
-
-
 class AInsuranceUserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = AInsuranceUserProfile
         fields = '__all__'
-        # exclude = ['token','registrationdate',]
-
+        #exclude = ['token','registrationdate',]
 
 class AUserbankDetailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = AUserbankDetails
-        fields = ['insuranceuserprofile', ]
-
+        fields = ['insuranceuserprofile',]
 
 class AUserworkDetailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = AUserworkDetails
-        fields = ['insuranceuserprofile', ]
-
+        fields = ['insuranceuserprofile',]
 
 class AMotorInsuranceSerializer(serializers.ModelSerializer):
     class Meta:
         model = AMotorInsurance
-        fields = ['insuranceuserprofile', ]
+        fields = '__all__'#['insuranceuserprofile',]
+#MotorUpdateRecord
+class MotorUpdateRecord(serializers.ModelSerializer):
+    class Meta:
+        model = MotorUpdateRecord
+        fields = ['insuranceuserprofile',]
 
+class PremiumMotorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PremiumMotor
+        fields = ['insuranceuserprofile',]
+
+class ClaimMotorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ClaimMotor
+        fields = ['insuranceuserprofile',]
+
+class PaymentReceiptSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PaymentReceipt
+        fields = ['insuranceuserprofile',]
+
+class CreditReceiptSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CreditReceipt
+        fields = ['insuranceuserprofile',]
 
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
-        fields = ['username',  'linenos', 'created']
-
+        fields = [ 'username',  'linenos','created']
+        
 class TipsSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Tips
