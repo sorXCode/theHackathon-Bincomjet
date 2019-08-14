eb56657658f76dee6582b5f9b7b3610428548e64
Christopher Akaninyene Okoro <uraniumkid30@gmail.com>
2019-08-14 14:42:54 +0100

    Update views.py

--- insuretech_apis/views.py
+++ insuretech_apis/views.py
@@ -10,7 +10,18 @@ from .models import *
 import json
 from .serializers import *
 from django.views.decorators.csrf import csrf_exempt
-
+import sendgrid
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+apikey='''yourkey'''
+client = SendGridAPIClient(apikey)
+def send_mail(email,subject,body,client=client):
+    message = Mail(
+        from_email='techinsure@bincom.com',
+        to_emails=email,
+        subject=subject,
+        html_content=body)
+    client.send(message)
 def get_user(request):
     try:
         token = request.data.copy().get('token') if not request.POST else request.POST.copy().get('token')
@@ -20,39 +31,10 @@ def get_user(request):
         return None
 
 @csrf_exempt
-@api_view(['POST', ])
+@api_view(['POST',])
 def profile_update_api(request):
-<<<<<<< HEAD
-    try:
-        token = request.data.copy().get(
-            'token') if not request.POST else request.POST.copy().get('token')
-        snippets = AInsuranceUserProfile.objects.get(token=token)
-    except AInsuranceUserProfile.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-||||||| merged common ancestors
-    try:
-        token = request.data.copy().get('token') if not request.POST else request.POST.copy().get('token')
-        snippets = AInsuranceUserProfile.objects.get(token=token)
-    except AInsuranceUserProfile.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-=======
     snippets = get_user(request)
->>>>>>> 8e59c708f66320656158e95cf4e37d6ee0463525
     if request.method == 'POST':
-<<<<<<< HEAD
-        serializer = AInsuranceUserProfileSerializer(
-            snippets, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-||||||| merged common ancestors
-        serializer = AInsuranceUserProfileSerializer(snippets, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-=======
         if snippets:
             serializer = AInsuranceUserProfileSerializer(snippets, data=request.data)
             if serializer.is_valid():
@@ -61,85 +43,39 @@ def profile_update_api(request):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         else:
             return Response(status=status.HTTP_404_NOT_FOUND)
->>>>>>> 8e59c708f66320656158e95cf4e37d6ee0463525
-
 
 @csrf_exempt
-@api_view(['POST', ])
+@api_view(['POST',])
 def profile_api(request):
-<<<<<<< HEAD
-    try:
-        token = request.data.copy().get(
-            'token') if not request.POST else request.POST.copy().get('token')
-        snippets = AInsuranceUserProfile.objects.get(token=token)
-        print('success')
-    except AInsuranceUserProfile.DoesNotExist:
-        print('failed')
-        return Response(status=status.HTTP_404_NOT_FOUND)
-||||||| merged common ancestors
-    try:
-        token = request.data.copy().get('token') if not request.POST else request.POST.copy().get('token')
-        snippets = AInsuranceUserProfile.objects.get(token=token)
-        print('success')
-    except AInsuranceUserProfile.DoesNotExist:
-        print('failed')
-        return Response(status=status.HTTP_404_NOT_FOUND)
-=======
     snippets = get_user(request)
->>>>>>> 8e59c708f66320656158e95cf4e37d6ee0463525
     if request.method == 'POST':
-<<<<<<< HEAD
-        serializer = AInsuranceUserProfileSerializer(
-            snippets, data=request.data)
-        if serializer.is_valid():
-            # serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-||||||| merged common ancestors
-        serializer = AInsuranceUserProfileSerializer(snippets,data=request.data)
-        if serializer.is_valid():
-            #serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-=======
         if snippets:
             serializer = AInsuranceUserProfileSerializer(snippets,)
-            if serializer.is_valid():
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, )
         else:
             return Response(status=status.HTTP_404_NOT_FOUND)
->>>>>>> 8e59c708f66320656158e95cf4e37d6ee0463525
-
 
 @csrf_exempt
-@api_view(['POST', ])
+@api_view(['POST',])
 def profile_login_api(request):
-    # works
+    #works
     try:
-<<<<<<< HEAD
-        token = request.data.copy().get(
-            'token') if not request.POST else request.POST.copy().get('token')
-        snippets = AInsuranceUserProfile.objects.get(username=token)
-||||||| merged common ancestors
-        token = request.data.copy().get('token') if not request.POST else request.POST.copy().get('token')
-        snippets = AInsuranceUserProfile.objects.get(username=token)
-=======
         email = request.data.copy().get('email') if not request.POST else request.POST.copy().get('email')
         snippets = AInsuranceUserProfile.objects.get(username=email)
->>>>>>> 8e59c708f66320656158e95cf4e37d6ee0463525
     except AInsuranceUserProfile.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
     if request.method == 'POST':
         serializer = AInsuranceUserProfileSerializer(snippets)
         return Response(serializer.data,)
 
-
 @csrf_exempt
-@api_view(['POST', ])
+@api_view(['POST',])
 def profile_create_api(request):
     if request.method == 'POST':
-        # works
+        email = request.data.copy().get('email') if not request.POST else request.POST.copy().get('email')
+        subject = 'INSURE Tech'
+        message = 'Welcome to insure '
+        send_mail(email,subject,message)
         serializer = AInsuranceUserProfileSerializer(data=request.data)
         print(serializer)
         if serializer.is_valid():
@@ -147,43 +83,7 @@ def profile_create_api(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
 @api_view(['GET', 'POST'])
-<<<<<<< HEAD
-def bankdetails_api(request):
-    if request.method == 'GET':
-        snippets = AUserbankDetails.objects.all()
-        serializer = AUserbankDetailsSerializer(snippets, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        serializer = AUserbankDetailsSerializer(data=request.data)
-        print(serializer)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['GET', 'POST'])
-||||||| merged common ancestors
-def bankdetails_api(request):
-    if request.method == 'GET':
-        snippets = AUserbankDetails.objects.all()
-        serializer = AUserbankDetailsSerializer(snippets, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        serializer = AUserbankDetailsSerializer(data=request.data)
-        print(serializer)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET', 'POST'])
-=======
->>>>>>> 8e59c708f66320656158e95cf4e37d6ee0463525
 def companydetails_api(request):
     if request.method == 'GET':
         snippets = AUserworkDetails.objects.all()
@@ -198,69 +98,6 @@ def companydetails_api(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-<<<<<<< HEAD
-
-@api_view(['GET', 'POST'])
-def motor_api(request):
-    if request.method == 'GET':
-        snippets = AMotorInsurance.objects.all()
-        serializer = AMotorInsuranceSerializer(snippets, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        serializer = AMotorInsuranceSerializer(data=request.data)
-        print(serializer)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['GET', 'POST'])
-def author_api(request):
-    if request.method == 'GET':
-        snippets = Author.objects.all()
-        serializer = AuthorSerializer(snippets, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        serializer = AuthorSerializer(data=request.data)
-        print(serializer)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-||||||| merged common ancestors
-@api_view(['GET', 'POST'])
-def motor_api(request):
-    if request.method == 'GET':
-        snippets = AMotorInsurance.objects.all()
-        serializer = AMotorInsuranceSerializer(snippets, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        serializer = AMotorInsuranceSerializer(data=request.data)
-        print(serializer)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET', 'POST'])
-def author_api(request):
-    if request.method == 'GET':
-        snippets = Author.objects.all()
-        serializer = AuthorSerializer(snippets, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        serializer = AuthorSerializer(data=request.data)
-        print(serializer)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-=======
 @csrf_exempt
 @api_view(['POST',])
 def bankdetails_create_api(request):
@@ -269,7 +106,7 @@ def bankdetails_create_api(request):
         if insurance_user:
             dummy = request.data
             del dummy['token']
-            model_data = {'insuranceuserprofile', insurance_user}
+            model_data = {'insuranceuserprofile': insurance_user.pk}
             model_data.update(dummy)
             serializer = AUserbankDetailsSerializer(data=model_data)
             print(serializer)
@@ -279,26 +116,7 @@ def bankdetails_create_api(request):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         else:
             return Response(status=status.HTTP_404_NOT_FOUND)
->>>>>>> 8e59c708f66320656158e95cf4e37d6ee0463525
 
-<<<<<<< HEAD
-
-'''class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    print(queryset)
-    serializer_class = UserSerializer
-||||||| merged common ancestors
-'''class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    print(queryset)
-    serializer_class = UserSerializer
-=======
 @csrf_exempt
 @api_view(['POST',])
 @renderer_classes([JSONRenderer])
@@ -312,7 +130,6 @@ def bankdetails_view_api(request,):
         else:
             content = {'status': 'not_found', 'data': {}}
         return Response(content)
->>>>>>> 8e59c708f66320656158e95cf4e37d6ee0463525
 
 @csrf_exempt
 @api_view(['POST',])
@@ -322,7 +139,7 @@ def companydetails_create_api(request):
         if insurance_user:
             dummy = request.data
             del dummy['token']
-            model_data = {'insuranceuserprofile', insurance_user}
+            model_data = {'insuranceuserprofile': insurance_user.pk}
             model_data.update(dummy)
             serializer = AUserworkDetailsSerializer(data=model_data)
             print(serializer)
@@ -347,15 +164,6 @@ def companydetails_view_api(request,):
             content = {'status': 'not_found', 'data': {}}
         return Response(content)
 
-<<<<<<< HEAD
-
-def odd(request):
-    pass
-||||||| merged common ancestors
-def odd(request):
-    pass
-=======
->>>>>>> 8e59c708f66320656158e95cf4e37d6ee0463525
 
 @csrf_exempt
 @api_view(['POST',])
@@ -365,7 +173,7 @@ def auto_insurance_create_api(request):
         if insurance_user :
             dummy = request.data
             del dummy['token']
-            model_data = {'insuranceuserprofile', insurance_user}
+            model_data = {'insuranceuserprofile': insurance_user.pk}
             model_data.update(dummy)
             serializer = AMotorInsuranceSerializer(data=model_data)
             print(serializer)
@@ -376,10 +184,6 @@ def auto_insurance_create_api(request):
         else:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
-<<<<<<< HEAD
-||||||| merged common ancestors
-
-=======
 @csrf_exempt
 @api_view(['POST',])
 @renderer_classes([JSONRenderer])
@@ -397,9 +201,8 @@ def auto_insurance_view_api(request,):
         return Response(content)
         #return HttpResponse(json.dumps(dj),content_type='text/json') works
 
->>>>>>> 8e59c708f66320656158e95cf4e37d6ee0463525
 class GeneralTipsViewSet(viewsets.ModelViewSet):
-    queryset = Tips.objects.filter(insurance_type="general")[:10]
+    queryset = Tips.objects.filter(insurance_type="general")
     serializer_class = TipsSerializer
 
 
@@ -407,11 +210,9 @@ class AutoTipsViewSet(viewsets.ModelViewSet):
     queryset = Tips.objects.filter(insurance_type="auto")
     serializer_class = TipsSerializer
 
-
 class LifeTipsViewSet(viewsets.ModelViewSet):
     queryset = Tips.objects.filter(insurance_type="life")
     serializer_class = TipsSerializer
-
 
 class HealthTipsViewSet(viewsets.ModelViewSet):
     queryset = Tips.objects.filter(insurance_type="health")
